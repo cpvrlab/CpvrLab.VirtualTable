d7fb8a7714f4c8c7b65d43f003d7504f97c79f08
wacki <wackigit@gmail.com>
2016-07-27 12:02:09 +0200

    Added leapmotion libraries and first version of oculus rift support

---
+++ Assets/VirtualTable/Scripts/GameManagement/PlayersAndInput/RiftPlayer.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityStandardAssets.Characters.FirstPerson;
+
+namespace CpvrLab.VirtualTable {
+
+
+    /// <summary>
+    /// Player using the oculus rift
+    /// </summary>
+    public class RiftPlayer : GamePlayer {
+
+        [Header("Rift Player Properties")]
+        public GameObject head;
+        public GameObject cam;
+
+        public override void OnStartClient()
+        {
+            base.OnStartClient();
+        }
+
+        public override void OnStartLocalPlayer()
+        {
+            base.OnStartLocalPlayer();
+            cam.SetActive(true);
+        }
+        
+        void Update()
+        {
+            if(!isLocalPlayer)
+                return;
+
+            head.transform.position = cam.transform.position;
+            head.transform.rotation = cam.transform.rotation;
+        }
+
+        protected override PlayerInput GetMainInput()
+        {
+            return null;
+        }
+
+        protected override void OnEquip(AttachmentSlot slot)
+        {
+        }
+        
+        protected override void OnUnequip(UsableItem item)
+        {
+        }
+        
+    } // class
+    
+} // namespace
