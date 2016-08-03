d0b62b13593959726726ed602daef33ca35790d8
wacki <wackigit@gmail.com>
2016-08-03 17:02:32 +0200

    more control panel stuff

---
+++ Assets/VirtualTable/Scripts/GUI/ControlPanelMenu.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CpvrLab.VirtualTable
+{
+
+    public class ControlPanelMenu : MonoBehaviour
+    {
+        public GameObject localPlayerSettings;
+        public GameObject SpectatorSettings;
+        public GameObject GameSettings;
+        public GameObject adminSettings;
+        public GameObject container;
+
+        public float edgeActivationRange = 10.0f;
+
+
+        private bool _isVisible = false;
+        private Animator _animator;
+
+        void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        void Update()
+        {
+            UpdateVisible();
+        }
+
+        void UpdateVisible()
+        {
+            var x = Input.mousePosition.x;
+            var y = Input.mousePosition.y;
+
+            if (_isVisible)
+            {
+                var rect = container.GetComponent<RectTransform>().rect;
+                if (x < 0.0f || rect.width < x || y < 0.0f || rect.height < y)
+                    HideMenu();
+            } else
+            {
+                if (!(x < 0.0f || edgeActivationRange < x || y < 0.0f || Screen.height < y))
+                    ShowMenu();
+            }
+        }
+        
+
+        void HideMenu()
+        {
+            Debug.Log("hide");
+            _isVisible = false;
+            //_animator.Stop();
+            _animator.SetBool("isVisible", _isVisible);
+        }
+
+        void ShowMenu()
+        {
+            Debug.Log("show");
+            _isVisible = true;
+            //_animator.Stop();
+            _animator.SetBool("isVisible", _isVisible);
+        }
+    }
+
+}
